2ae49bfd7163858a3b31b336e6456256be6394f2
xknxknqq <1553730177@qq.com>
2018-04-01 17:47:53 +0800

    rm

--- mc16.4/AIMStarterKitPC/AdventuresInMinecraft/StartBukkit.bat
+++
@@ -1,9 +0,0 @@
-ECHO OFF
-ECHO "Adventures In Minecraft"
-ECHO "Bukkit Minecraft Server Version is 1.6.4"
-ECHO "  Note - make sure Minecraft is using version 1.6.4"
-pause
-cd Bukkit
-start.exe
-pause
-ECHO ON
